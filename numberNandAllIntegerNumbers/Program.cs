a250be277d99c125f9adb575ac7fba7f47d3392b
SergeyCherevichenko <cherevichenkosn@gmail.com>
2023-07-05 22:37:18 +0500

    Диапозон чисел от -N до N

---
+++ numberNandAllIntegerNumbers/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите целое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+numberA = Math.Abs(numberA);
+int numberB = -numberA;
+while(numberB <= numberA)
+{
+ Console.Write(numberB + " ");
+ numberB++;    
+}

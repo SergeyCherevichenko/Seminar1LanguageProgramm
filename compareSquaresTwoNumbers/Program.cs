97636435ac1aa8a9b8057e061af42de6bfcef647
SergeyCherevichenko <cherevichenkosn@gmail.com>
2023-07-05 13:07:25 +0500

    Сравнение квадратов двух чисел

---
+++ compareSquaresTwoNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите первое число: ");
+int numberX = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numberY = Convert.ToInt32(Console.ReadLine());
+if(numberX * numberX  == numberY )
+{ 
+Console.Write("Ваше первое число является квадратом второго : ");
+Console.Write(numberX);
+Console.Write(" * ");
+Console.Write(numberX);
+Console.Write(" = ");
+Console.Write(numberY); 
+}
+else
+{
+    Console.Write("Ваше первое число не является квадратом второго: " );
+    Console.Write(numberX);
+    Console.Write(" * ");
+    Console.Write(numberX);
+    Console.Write(" не = ");
+    Console.Write(numberY);
+}
